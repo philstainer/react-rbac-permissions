f86d380e7864716d480f1cc2313c3ade235f21d1
Phil Stainer <me@philstainer.io>
2026-01-05 10:09:56 +0000

    chore: update build format and platform config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'tsdown'
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  platform: 'neutral',
-  // ...config options
-})
+  platform: "browser",
+  format: "esm",
+});
